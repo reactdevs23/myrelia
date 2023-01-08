7858d236508c0ae0014e77499ac1a6e139557e6e
dalimkumar604 <dalimkumar212582@gmail.com>
2023-01-09 01:31:56 +0600

    copied

--- src/components/Home/Header/Header.js
+++ src/components/Home/Header/Header.js
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import CopyToClipboard from "react-copy-to-clipboard";
 import { AiOutlineClose } from "react-icons/ai";
 import { IoIosPeople } from "react-icons/io";
 import styles from "./Header.module.css";
 
 const Header = () => {
+  const [copied, setCopied] = useState(false);
   return (
     <div className={styles.wrapper}>
-      <div className={styles.play}>
-        <div>
-          <p className={styles.text}>This server is ready! </p>
-          <h6 className={styles.heading}>PLAY.MYRELIA</h6>
+      <CopyToClipboard
+        text="PLAY.MYRELIA"
+        onCopy={() => setCopied((prev) => !prev)}
+      >
+        <div className={`${styles.play} ${copied && styles.copy}`}>
+          <div>
+            <p className={styles.text}>This server is ready! </p>
+            <h6 className={styles.heading}>PLAY.MYRELIA</h6>
+          </div>
+          <AiOutlineClose className={styles.icon} />
         </div>
-        <AiOutlineClose className={styles.icon} />
-      </div>
+      </CopyToClipboard>
       <div className={styles.buttonContainer}>
         <button className={styles.button}>SHOP</button>
       </div>
